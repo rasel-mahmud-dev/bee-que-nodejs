88359291329a50adcd039ba516fa7d87616b7ca1
Rasel Mahmud <rasel.mahmud.dev@gmail.com>
2023-10-22 22:29:16 +0600

    queue implement using in redis

--- rs-queue.js
+++ rs-queue.js
@@ -7,53 +7,74 @@ const client = redis.createClient({
 
 class RsQueue extends EventEmitter {
     option = {
-        queueName: ""
+        jobsKey: "rq:jobs", queueKey: "rq:queue", doneKey: "rq:done", queue: "rq:queue"
     }
 
-    constructor(props) {
-        super(props)
-        this.queue = {
-            active: [],
-            done: {},
-            jobs: {},
-            error: {}
+    constructor(queueName) {
+        super(queueName)
+        this.state = {
+            done: {}, queue: [], jobs: {}, error: {}
         }
 
-
-        if (props?.queueName) {
-            this.option.queueName = props.queueName
+        if (queueName) {
+            this.option.jobsKey = `rq:${queueName}:jobs`
+            this.option.queueKey = `rq:${queueName}:queue`
+            this.option.doneKey = `rq:${queueName}:done`
+            this.option.queueName = queueName.queueName
         }
 
         this.connectRedis()
-
-
     }
 
     async connectRedis() {
-        await client.connect()
-        this.client = client
+        try {
+            await client.connect()
+            this.client = client
+            this.state.jobs = await client.hGetAll(this.option.jobsKey);
+            await client.lTrim(this.option.queueKey, 1, 0)
 
-        if (this.queue.active.length) {
-            this.interval(this.queue.active)
+            let jobKeys = Object.keys(this.state.jobs);
+
+            for (let jobKey of jobKeys) {
+                await client.lPush(this.option.queueKey, jobKey)
+                this.state.queue.push(jobKey)
+            }
+
+            let listLen = await client.lLen(this.option.queueKey)
+            if (listLen) {
+                this.interval(this.state.queue)
+            }
+        } catch (ex) {
+            console.log(ex?.message)
         }
-
     }
 
     intervalId = 0;
 
     async createJob(job) {
-        this.queue.jobs[job.id] = JSON.stringify(job)
-        this.queue.active.push(job.id)
-        this.emit("data", this.queue.jobs[job.id])
-        this.queueProcess(this.queue.jobs)
+        try {
+            await client.hSet(this.option.jobsKey, {
+                [job.id]: JSON.stringify(job)
+            })
+            this.state.jobs[job.id] = JSON.stringify(job)
+
+            if (!this.state.queue.includes(job.id)) {
+                await client.lPush(this.option.queueKey, job.id)
+                this.state.queue.push(job.id)
+            }
+
+            this.emit("data", this.state.jobs[job.id])
+            this.queueProcess()
+        } catch (ex) {
+            console.log(ex?.message)
+        }
     }
 
     slats() {
-        const doneCount = Object.keys(this.queue.done).length;
-        const pendingCount = Object.keys(this.queue.jobs).length;
-        const errorCount = Object.keys(this.queue.error).length;
+        const doneCount = Object.keys(this.state.done).length;
+        const pendingCount = Object.keys(this.state.jobs).length;
+        const errorCount = Object.keys(this.state.error).length;
 
-        // Define colors using ANSI escape codes
         const green = "\x1b[32m";
         const yellow = "\x1b[33m";
         const red = "\x1b[31m";
@@ -67,33 +88,44 @@ class RsQueue extends EventEmitter {
     }
 
     interval() {
-        clearInterval(this.intervalId);
-        this.intervalId = setTimeout(() => {
-            this.queueProcess(this.queue.jobs)
-        }, 2000)
+        try {
+            clearInterval(this.intervalId);
+            this.intervalId = setTimeout(async () => {
+                await this.queueProcess(this.state.jobs)
+            }, 3000)
+        } catch (ex) {
+            console.log(ex?.message)
+        }
     }
 
-    queueProcess(jobs) {
-        let firstTask = this.queue.active[0]
-        this.emit("process", jobs[firstTask], function (isDone) {
-            if (isDone) {
-                let doneJob = jobs[firstTask]
-                this.queue.done[firstTask] = doneJob
-                delete jobs[firstTask]
-                this.queue.active.shift()
-            } else {
-                this.queue.active.shift()
-                this.queue.active.push(firstTask)
-                this.interval(this.queue.active)
-            }
+    async queueProcess() {
+        try {
+            const jobs = this.state.jobs;
 
-            this.emit("done", this.queue)
+            let firstTask = await client.lPop(this.option.queueKey)
 
-        }.bind(this))
+            this.emit("process", jobs[firstTask], async function (isDone) {
+                if (isDone) {
+                    delete this.state.jobs[firstTask]
+                    await client.hDel(this.option.jobsKey, firstTask)
+                    await client.lPush(this.option.doneKey, firstTask)
+                    this.state.done[firstTask] = jobs[firstTask]
+
+                } else {
+                    await client.rPush(this.option.queueKey, firstTask)
+                    this.interval()
+                }
+
+                this.emit("done", this.state)
+
+            }.bind(this))
+        } catch (ex) {
+            console.log(ex?.message)
+        }
     }
 }
 
-let queue = new RsQueue({queueName: "test"})
+let queue = new RsQueue("test")
 
 queue.on("data", function (data) {
     console.log(data)
@@ -105,20 +137,19 @@ queue.on("process", function (data, done) {
 })
 
 queue.on("done", function (data) {
-    console.log("Done job", data)
+    // console.log("Done job", data)
 })
 
 queue.slats()
 
 function createTask(name, id) {
     queue.createJob({
-        id: id,
-        data: name
+        id: id, data: name
     })
 }
 
 async function makeTransaction(name) {
-    const id = name + "--" + Date.now();
+    const id = name + "--";
     if (false) {
         // create transaction ...
         insertInDatabase(name, id)
@@ -148,8 +179,7 @@ function insertInDatabase(data, taskId) {
 const readline = require("readline");
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+    input: process.stdin, output: process.stdout
 });
 
 function askUser() {
